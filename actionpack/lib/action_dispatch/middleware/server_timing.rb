082d772218c9365d9b41961a738688d5bf0f831b
mihaic195 <colceriumi@gmail.com>
2022-01-17 16:28:15 +0200

    revert

--- actionpack/lib/action_dispatch/middleware/server_timing.rb
+++ actionpack/lib/action_dispatch/middleware/server_timing.rb
@@ -23,10 +23,6 @@ module ActionDispatch
       end
 
       header_info = events.group_by(&:name).map do |event_name, events_collection|
-        p "------"
-        p "#{event_name}"
-        p "#{events_collection}"
-        p "------"
         "#{event_name};dur=#{events_collection.sum(&:duration)}"
       end
       headers[SERVER_TIMING_HEADER] = header_info.join(", ")
